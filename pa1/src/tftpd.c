6a44bd9e0878ed255146c9b8e156a5be6468e112
gunnlaugur15 <gunnlaugur15@skel.ru.is>
2017-09-05 14:36:52 +0000

    builds

--- pa1/src/tftpd.c
+++ pa1/src/tftpd.c
@@ -4,14 +4,15 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
 	
-	int socket;
+	int sock;
 	struct sockaddr_in server, client;
 	int portNumber;
-        ssize_t pack;
-        char message[512];
+    ssize_t pack;
+    char message[512];
 	if(argc < 2 ||argc > 3){
 	    return 0;
 	}
@@ -22,26 +23,29 @@ int main(int argc, char *argv[])
 	    portNumber = 63479; //Default port Frilla
 	}
 	//Create the socket, check if success
-	if((socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-		close(socket);
+	if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+		//close(sock);
 		exit(0);
 	}
 	//Allocate memory for server
 	memset(&server, 0, sizeof(server));
 	server.sin_family = AF_INET; //WE ARE FAMI LY
-	server.sin_addr = htonl(INADDR_ANY); //Get long integer, convert to Network byte order
+	server.sin_addr.s_addr = htonl(INADDR_ANY); //Get long integer, convert to Network byte order
 	server.sin_port = htons(portNumber); //Set port number for server
 	
 	//Bind Socket to socket address, exit if fail
-	if(bind(socket, (struct sockaddr *) &server, sizeof(struct sockaddr_in)) < 0){
-	    close(socket);
+	if(bind(sock, (struct sockaddr *) &server, sizeof(struct sockaddr_in)) < 0){
+	    //close(sock);
 	    exit(0);
 	}
 
 	while(1){
-	    ssize_t pack;
-	    pack = recvfrom(socket, message, 1024, 0, (struct sockaddr *) &client, sizeof(struct sockaddr_in));
-	    if(pack < 0){
+
+		socklen_t clientlen = sizeof(struct sockaddr_in);
+	    
+		pack = recvfrom(sock, message, 1024, 0, (struct sockaddr *)&client, &clientlen);
+	    
+		if(pack < 0){
 	      //Pakki nadist ekki
 	      //tharf ad returna error
 	      exit(0);
